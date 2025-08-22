2f2804b95b2953ca8c991bfcc213919114606a12
ZuhalDesign01 <zainabzohalbalkhi@gmail.com>
2025-08-22 18:51:58 +0430

    update

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 ///////////////////////////////////////
 // Functions
-function logger() {
+/*function logger() {
   console.log("My name is Zuhal");
 }
 
@@ -34,3 +34,39 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number("23");
+*/
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function declaration
+/*function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+*/
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Zuhal"));
+console.log(yearsUntilRetirement(1980, "Bob"));
